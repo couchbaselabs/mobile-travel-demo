595ffd52e09828212bc958ed310b28c9c743da51
Austin Gonyou <agonyou@gmail.com>
2020-02-04 16:33:19 -0800

    Updated Dockerfile to use .deb for sync gateway instead of tar.gz Updated the create_hotels_fts.sh with additional control.

--- try-cb-python/create_hotels_fts.sh
+++ try-cb-python/create_hotels_fts.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-curl -u Administrator:password -XPUT http://localhost:8094/api/index/hotels  -H 'cache-control: no-cache'  -H 'content-type: application/json'  -d@./fts-hotels-index.json
+echo "This script will make the full text index needed for the travel-sample"
+ 
+userName="Administrator"
+userPass="password"
+ftsRes="n"
+echo
+read -rsn1 -p "Proceeed? y/(n)" ftsRes 
+
+if [ $ftsRes = "y" ] || [ $ftsRes = "Y" ] 
+then
+    curl -u "$userName":"$userPass" -XPUT http://localhost:8094/api/index/hotels  -H 'cache-control: no-cache'  -H 'content-type: application/json'  -d@./fts-hotels-index.json
+else
+    echo "FTS Index create cancelled. Exiting ..."
+fi 
+
